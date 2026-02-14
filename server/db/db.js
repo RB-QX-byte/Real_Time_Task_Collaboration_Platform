60c5b6a222df642f7c7a7c907d93baed48a51a5a
RB-QX-byte <rachitborkar7@gmail.com>
2026-02-15 01:21:41 +0530

    initialized  backend for real-time task collaboration platform
    - Updated package.json with project name, description, author, and added scripts for testing and development.
    - Created db.js for MongoDB connection handling.
    - Added index.js as the entry point for the server.
    - Implemented app.js to set up Express server with CORS and JSON parsing.

---
+++ server/db/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+await async function connectDB() {
+    // Proper Connection to MongoDB
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB");
+    } 
+    
+    catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
